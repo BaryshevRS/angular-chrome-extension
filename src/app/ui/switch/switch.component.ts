16adda45a921f2d905b9e26c1e3e5e6cdf31bae5
BaryshevRS <baryshevrs@yandex.ru>
2022-01-06 17:45:04 +0300

    Added styles for popup component

--- src/app/ui/switch/switch.component.ts
+++ src/app/ui/switch/switch.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+type SwitchSize = 'mini' | 'default'
 
 @Component({
   selector: 'app-switch',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./switch.component.scss']
 })
 export class SwitchComponent implements OnInit {
+
+  @Input() size: SwitchSize = 'default'
 
   constructor() { }
 
